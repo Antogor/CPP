4bbaab528eef553c4b73714be4db2273de6573bf
Antogor <antoniogarzoniniesta@gmail.com>
2020-07-28 18:08:10 +0200

    init ex02

---
+++ Module00/ex02/Account.class.cpp
@@ -0,0 +1,91 @@
+#include "Account.class.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit){
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_totalAmount += initial_deposit;
+	this->_accountIndex = this->_nbAccounts++;
+	//metodo que de la fecha
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
+
+Account::Account(){
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_accountIndex = this->_nbAccounts++;
+	//metodo que de la fecha
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created" << std::endl;
+}
+
+Account::~Account(){
+	this->_nbAccounts--;
+	//metodo de la hora
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+int Account::getNbAccounts(){
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount(){
+	return _totalAmount;
+}
+
+int Account::getNbDeposits(){
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(){
+	return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(){
+	//metodo de la hora
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void Account::makeDeposit(int deposit){
+	//escribirlo en pantalla
+	this->_amount += deposit;
+	this->_nbDeposits++;
+}
+
+bool Account::makeWithdrawal(int withdrawal){
+	//escribirlo en pantalla
+	if (this->_amount < withdrawal)
+		return false;
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	return true;
+}
+
+int Account::checkAmount()const{
+}
+
+void Account::displayStatus() const{
+	//metodo de la hora
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;	
+}
+
+void Account::_displayTimestamp(void){
+}
