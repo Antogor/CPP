ff7a9741af920de23abf737e93ffe0475f4fc8d2
Antogor <antoniogarzoniniesta@gmail.com>
2022-06-04 17:54:57 +0200

    add expecifications to main ex00 Module07

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 17:48:47 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/09/10 16:03:56 by agarzon-         ###   ########.fr       */
+/*   Updated: 2022/06/04 17:53:15 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,25 @@ int main( void ) {
     int a = 2;
     int b = 3;
 
+	std::cout << "Value of int A: "<< a << std::endl;
+	std::cout << "Value of int B: "<< b << std::endl;
+	std::cout << "SWAP A and B" << std::endl;
     ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::cout << "Value of a = " << a << ", value of b = " << b << std::endl;
+    std::cout << "Value min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "Value max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    std::string c = "chaine1";
+	std::cout << "-------------" << std::endl;
+    
+	std::string c = "chaine1";
     std::string d = "chaine2";
     
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    return 0;
+	std::cout << "Value of string C: "<< c << std::endl;
+	std::cout << "Value of string D: "<< d << std::endl;
+	std::cout << "SWAP C and D" << std::endl;
+	::swap(c, d);
+    std::cout << "Value of c = " << c << ", Value of d = " << d << std::endl;
+    std::cout << "Value min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "Value max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -4,18 +4,18 @@
 # include <string>
 # include <iostream>
 
-template <class T> void swap (T &a, T &b){
+template <typename T> void swap (T &a, T &b){
 	T tmp;
 	tmp = b;
 	b = a;
 	a = tmp;
 }
 
-template <class T> T min (T a, T b){
+template <typename T> T min (T a, T b){
 	return a == b ? b : a < b ? a : b;
 }
 
-template <class T> T max (T a, T b){
+template <typename T> T max (T a, T b){
 	return a == b ? b : a < b ? b : a;
 }
 
