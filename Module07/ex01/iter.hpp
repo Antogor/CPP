11e5b5a35ef8f352d824b0716e1958f2e967c6f5
Antogor <antoniogarzoniniesta@gmail.com>
2022-06-04 18:04:38 +0200

    add some expecifications to main ex01 module07

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -4,15 +4,15 @@
 # include <string>
 # include <iostream>
 
-template <class T> void inc(T &t){
+template <typename T> void inc(T &t){
 	t++;
 }
 
-template <class T> void imp(T t){
+template <typename T> void imp(T t){
 	std::cout << t << std::endl;
 }
 
-template <class P, class L, class F, class T> void iter(P *p, L l, F (*f)(T t)){
+template <typename P, typename L, typename F, typename T> void iter(P *p, L l, F (*f)(T t)){
 
 	for (int i = 0; i < l; i++){
 		f(p[i]);
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 17:48:47 by agarzon-          #+#    #+#             */
-/*   Updated: 2020/09/10 16:03:56 by agarzon-         ###   ########.fr       */
+/*   Updated: 2022/06/04 18:02:43 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,22 @@ int main( void ) {
     int l = 5;
     int iarr[l];
 
-    std::cout << "--- ARRAY OF INTS ---" << std::endl;  
+    std::cout << "--- CREATING ARRAY OF INTS ---" << std::endl;  
     for(int i = 0; i < l; i++){
         iarr[i] = 1 + std::rand() % (l-1);
-        std::cout << "Int stroed: " << iarr[i] << std::endl;
+        std::cout << "Int stored: " << iarr[i] << std::endl;
     }
 
+    std::cout << "--- USING INCREMENT ARRAY FUNCTION ---" << std::endl;
     ::iter(iarr, l, inc<int>);
 
     std::cout << "--- ARRAY AFTER INCREMENT ---" << std::endl;
     ::iter(iarr, l, imp<int>);
 
-    std::cout << "--- ARRAY OF CHARS ---" << std::endl;
+    std::cout << "\n--- CREATING ARRAY OF CHARS ---" << std::endl;
     char charr[5] = {'a', 'b', 'c', 'd', 'e'};
 
+    std::cout << "--- USING INCREMENT ARRAY FUNCTION ---" << std::endl;
     ::iter(charr, 5, imp<char>);
 
     std::cout << "--- ARRAY AFTER INCREMENT ---" << std::endl;
