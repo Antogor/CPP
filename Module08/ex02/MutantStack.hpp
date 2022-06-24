bc3d51fb82c3e96ebffe067bb5f0072c8d393ce3
Antonio Garzon iniesta <agarzon-@c1r9s6.42madrid.com>
2022-06-24 20:02:30 +0200

    Module08 init ex02

---
+++ Module08/ex02/MutantStack.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 20:00:23 by agarzon-          #+#    #+#             */
+/*   Updated: 2022/06/24 20:01:54 by agarzon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+# include <ostream>
+# include <iostream>
+# include <algorithm>
+# include <stack>
+# include <list>
+
+template < typename T >
+class MutantStack : public std::stack<T>
+{
+	private:
+
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+
+		MutantStack():std::stack<T>(){};
+		MutantStack(MutantStack const &other) : std::stack<T>(other){};
+		virtual ~MutantStack(){};
+
+		iterator begin(){ return std::stack<T>::c.begin(); }
+		iterator end(){ return std::stack<T>::c.end(); }
+
+		const_iterator cbegin() const { return std::stack<T>::c.cbegin(); };
+		const_iterator cend() const { return std::stack<T>::c.cend(); };
+
+		reverse_iterator rbegin(){ return std::stack<T>::c.rbegin(); };
+		reverse_iterator rend(){ return std::stack<T>::c.rend(); };
+
+		const_reverse_iterator crbegin() const { return std::stack<T>::c.crbegin(); };
+		const_reverse_iterator crend() const { return std::stack<T>::c.crend(); };
+			
+		MutantStack &operator=(MutantStack const &other)
+		{
+			std::stack<T>::operator=(other);
+			return (*this);
+		};
+			
+
+};
+
+#endif
---
+++ Module08/ex02/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 20:00:32 by agarzon-          #+#    #+#             */
+/*   Updated: 2022/06/24 20:01:58 by agarzon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main(){
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	
+	//[...]
+	
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	
+	++it;
+	--it;
+	
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	
+	std::stack<int> s(mstack);
+	
+	return 0;
+}
